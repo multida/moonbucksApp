9606767ca6ff5c5404f089478b3b068aaeab358c
김다예 <wow_da65@naver.com>
2023-01-14 11:23:31 +0900

    🚧 메뉴 수정하기

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
-/**
- * @TODO메뉴추가
- * 메뉴의 이름을 입력 받아서 확인, 엔터키를 누르면 메뉴가 추가 된다.
- * <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 <li>로  메뉴명이 생긴다. ( + 수정, 삭제 버튼)
- * 메뉴가 추가되면 입력창이 빈값으로 바뀐다.
- * 입력창에 아무것도 없으면 추가되는 작업 막기.
- * 총 메뉴 개수 count 업데이트
- */
-
 const $ = (selector) => document.querySelector(selector);
+
+$("#espresso-menu-list").addEventListener("click", (e) => {
+  if (e.target.classList.contains("menu-edit-button")) {
+    const $menuName = e.target.closest("li").querySelector(".menu-name");
+    const editedMenuName = prompt(
+      "메뉴 이름을 수정하시겠습니까?",
+      $menuName.innerText
+    );
+    $menuName.innerText = editedMenuName;
+  }
+});
 
 //fomr 태그가 자동으로 전송되는걸 막기
 $("#espresso-menu-form").addEventListener("submit", (e) => {
@@ -41,10 +43,7 @@ const addMenuName = () => {
     `;
   };
 
-  // $("#espresso-menu-list").innerHTML = menuItemTemplate(espressoMenuName); -> 덮어쓰기 됨
-
   $("#espresso-menu-list").insertAdjacentHTML(
-    //beforebegin, afterbegin, beforeend, afterend
     "beforeend",
     menuItemTemplate(espressoMenuName)
   );
@@ -75,6 +74,7 @@ $("#espresso-menu-name").addEventListener("keypress", (e) => {
  * 수정 버튼을 누르면 prompt창이 나온다.
  * 입력창에 신규 메뉴를 입력하고, 확인을 누르면 수정된다.
  */
+
 /**
  * @TODO메뉴삭제
  * 삭제 버튼을 누르면 confirm창이 나온다.
